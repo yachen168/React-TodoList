78f36bbbc4ccc80efb890996b018fe38d4cc8c3a
yachen <io260188@gmail.com>
2020-12-25 22:43:20 +0800

    feat: Header

---
+++ src/components/header/index.jsx
@@ -0,0 +1,34 @@
+import './index.scss';
+
+const Header = ({ filterState, setFilterState }) => {
+  const navItems = [
+    {title: 'My Tasks', status: 'all'}, 
+    {title: 'In progress', status: 'inProgress'},
+    {title: 'Completed', status: 'completed'}];
+
+  const navItemHandler = (e) => {
+    const status = e.currentTarget.dataset.name;
+      setFilterState(status)
+    }
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          {navItems.map(item => {
+            return (
+              <li key={item.status}
+                  className={`nav-item ${filterState === item.status ? 'active' : ''}`} 
+                  data-name={item.status}
+                  onClick={navItemHandler}>
+                  {item.title}
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
