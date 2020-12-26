6fec1c4f9d0b365b444433636192d1e7883512d1
yachen <io260188@gmail.com>
2020-12-26 20:51:45 +0800

    fix: style for todoItem

--- src/components/todoList/index.jsx
+++ src/components/todoList/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from '../todoItem';
+import './index.scss';
 
 
 const TodoList = ({todos, setTodos}) => {
   
   return (
     <section className="todo-list">
-      {todos.map(todo => {
-        return (
-          <TodoItem key={todo.todoId} todo={todo} todos={todos} setTodos={setTodos}/>
-        )
+      {
+        todos.map(todo => {
+          return (
+            <TodoItem key={todo.todoId} todo={todo} todos={todos} setTodos={setTodos}/>
+          )
       })}
     </section>
   )
