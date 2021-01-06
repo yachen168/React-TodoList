7b2d255838feb1c9ce8ef67a05863a94b8e3672f
yachen <io260188@gmail.com>
2021-01-06 16:22:16 +0800

    fix: useEffect

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
 
   useEffect(() => {
     setLocalStorage();
-   }, [state]);
+   }, [state.todos]);
   
   const setLocalStorage = () => {
     window.localStorage.setItem('react-todo', JSON.stringify(state.todos));
